b09273affa91ee4f3e76dd565f0b7f7588bd1638
RyanChehab <ryanchehab20@gmail.com>
2024-12-15 22:37:33 +0200

    js <=> returned AuthContext provider

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,1 +1,13 @@
 import React,{useState,useEffect,createContext} from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children}) =>{
+    
+
+    return(
+        <AuthContext.Provider>
+            {children}
+        </AuthContext.Provider>
+    )
+}
