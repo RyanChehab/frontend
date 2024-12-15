4d82c8000955afd6be8092526dfbb940d1e0f37b
RyanChehab <ryanchehab20@gmail.com>
2024-12-15 03:06:29 +0200

    jsx <=> created login component

---
+++ src/components/login/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, TextField, Button, Typography } from '@mui/material';
+
+const Login = () => {
+    return (
+        <Box>
+           
+        </Box>
+    );
+};
+
+export default Login
+;
