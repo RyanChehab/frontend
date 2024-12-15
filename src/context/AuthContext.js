aeb10dd271c951020bcc14122f0d142a50f19eab
RyanChehab <ryanchehab20@gmail.com>
2024-12-15 23:28:58 +0200

    js <=> passed values to provider

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,10 +3,18 @@ import React,{useState,useEffect,createContext} from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) =>{
-    
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+
 
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{
+            email,
+            setEmail,
+            password,
+            setPassword
+            }}>
             {children}
         </AuthContext.Provider>
     )
