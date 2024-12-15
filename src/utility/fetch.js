6f1892447d8362f0d992cc964b0f009982d31885
RyanChehab <ryanchehab20@gmail.com>
2024-12-15 23:03:37 +0200

    js <=> returned api response

--- src/utility/fetch.js
+++ src/utility/fetch.js
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const fetchData = async(url,method = 'POST', data = null, headers={})=>{
     try{
-        
+        const response = await axios({
+            url,
+            method,
+            data,
+            headers,
+        });
+        return response.data;
+
     }catch(error){
-        
+                                               
     }
 }
