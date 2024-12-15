61a0223bcc91e4b1129570defca071bd1f24045e
RyanChehab <ryanchehab20@gmail.com>
2024-12-15 03:12:30 +0200

    jsx <=> designed the box

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -3,7 +3,16 @@ import { Box, TextField, Button, Typography } from '@mui/material';
 
 const Login = () => {
     return (
-        <Box>
+        <Box
+        sx={{
+            border: '3px solid #FC8E40',
+            borderRadius: '10px',
+            padding: '16px',
+            width: '450px',
+            height: '700px',
+            margin: '0 auto'
+        }}
+        >
            
         </Box>
     );
