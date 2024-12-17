01fd849729a3d1632ff8f4b2cf7b1aba80c3dc82
RyanChehab <ryanchehab20@gmail.com>
2024-12-17 15:51:37 +0200

    js <=> fetched login api

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,9 +6,24 @@ export const AuthContext = createContext();
 export const AuthProvider = ({children}) =>{
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [user,setUser] = useState('');
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const handleLogin = async ()=>{
+        setLoading(true);
+        setError(null);
+        try{
+            const result = await fetchData(
+                "http://localhost:8000/api/login",
+                "POST",
+                {email,password}
+            )
+            
+        }catch(error){
+
+        }
+    }
 
     return(
         <AuthContext.Provider value={{
