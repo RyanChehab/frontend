e4c0121717b688b65bfeda3cd4b9962413660cfc
RyanChehab <ryanchehab20@gmail.com>
2024-12-18 02:15:25 +0200

    jsx <=> created signup function

--- src/components/signup/index.jsx
+++ src/components/signup/index.jsx
@@ -2,3 +2,14 @@ import React,{useContext} from 'react';
 import './login.css';
 import '../../css/utilities.css';
 import { Box, TextField, Button, Typography, Snackbar, Alert, Slide} from '@mui/material';
+
+function SlideTransition(props) {
+    return <Slide {...props} direction="down" />;
+  }
+
+const Signup = () => {
+
+    return(
+        
+    )
+}
