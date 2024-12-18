2d9a42de7886545929bd8a3c226d6c155d904f8c
RyanChehab <ryanchehab20@gmail.com>
2024-12-18 11:21:56 +0200

    js <=> routed login and signup components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Login from "./components/login";
+import Signup from "./components/signup";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <AuthProvider>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
