718b59e132d4296e9afd80528f37143d800c6654
RyanChehab <ryanchehab20@gmail.com>
2024-12-22 20:10:45 +0200

    jsx <=> added logo

--- src/components/writerNav/index.jsx
+++ src/components/writerNav/index.jsx
@@ -1,15 +1,26 @@
 import {React,useContext} from 'react';
 import {Typography,Button,AppBar,Toolbar} from '@mui/material';
+import {Link} from 'react-router-dom';
 import InputBase from '@mui/material/InputBase';
 import { styled } from '@mui/material/styles';
-
+import logo from '../../assets/logo.png';
+import './writerNav.css';
 const WriterNav = ()=>{
     return (
-        <AppBar position="static">
-            <Toolbar>
-                
-            </Toolbar>
-        </AppBar>
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/" style={{ textDecoration: 'none'}}>
+                    <img src={logo} alt="logo" />
+                </Link>
+            </div>
+
+            <div className="nav-links">
+                <Link to="/repositories">Repositories</Link>
+                <Link to="/categories">Categories</Link>
+                <Link to="/bookmarks">Bookmarks</Link>
+            </div>
+            
+        </nav>
     );
 }
 export default WriterNav;
