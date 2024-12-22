6c202fe3ed8d68e23414c504c1aff8a902043ec0
RyanChehab <ryanchehab20@gmail.com>
2024-12-22 19:39:37 +0200

    jsx <=> imported inputBase and styled

--- src/components/writerNav/index.jsx
+++ src/components/writerNav/index.jsx
@@ -1,5 +1,15 @@
 import {React,useContext} from 'react';
+import {Typography,Button,AppBar,Toolbar} from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+import { styled } from '@mui/material/styles';
 
-const writerNav = ()=>{
-    
+const WriterNav = ()=>{
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+    );
 }
+export default WriterNav;
