acf2b81ccd2a1fdd0fe10d77e91d3ca4f9ec3e17
RyanChehab <ryanchehab20@gmail.com>
2024-12-22 20:16:56 +0200

    jsx <=> created searchBar

--- src/components/writerNav/index.jsx
+++ src/components/writerNav/index.jsx
@@ -5,6 +5,16 @@ import InputBase from '@mui/material/InputBase';
 import { styled } from '@mui/material/styles';
 import logo from '../../assets/logo.png';
 import './writerNav.css';
+
+const SearchBar = styled(InputBase)(({ theme }) => ({
+    width: '453px',
+    height: '46px',
+    padding: '0 10px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    fontSize: '16px',
+}));
+
 const WriterNav = ()=>{
     return (
         <nav className="navbar">
@@ -15,11 +25,11 @@ const WriterNav = ()=>{
             </div>
 
             <div className="nav-links">
-                <Link to="/repositories">Repositories</Link>
-                <Link to="/categories">Categories</Link>
-                <Link to="/bookmarks">Bookmarks</Link>
+                <Link to="/repositories" className='links'>Repositories</Link>
+                <Link to="/categories" className='links'>Categories</Link>
+                <Link to="/bookmarks" className='links'>Bookmarks</Link>
             </div>
-            
+
         </nav>
     );
 }
