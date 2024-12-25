2f2347ee09bed2afda9afefd885a0e5a1f7d3561
RyanChehab <ryanchehab20@gmail.com>
2024-12-25 23:25:01 +0200

    js <=> added functions

--- src/context/NavContext.js
+++ src/context/NavContext.js
@@ -7,11 +7,29 @@ const NavProvider = ({children})=>{
 const [isCollapsed, setIsCollapsed] = useState(false);
 const [anchorEl,setAnchorEl] = useState(null);
 
+// functions
 const handleOpen = (e)=>{
     setAnchorEl(e.currentTarget)
 }
 const handleClose = ()=>{
     setAnchorEl(false)
 }
+//responsiveness
+useEffect(() => {
+    const handleResize = () => {
+        if (window.innerWidth <= 1128) {
+            setIsCollapsed(true); 
+        } else {
+            setIsCollapsed(false);
+        }
+    };
 
+    handleResize();
+
+    window.addEventListener('resize', handleResize); 
+
+    return () => {
+        window.removeEventListener('resize', handleResize); 
+    };
+}, []);
 }
