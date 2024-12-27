607dce3ae1910f2972eb8fca484d0be1bc9fd841
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 17:56:31 +0200

    js <=> fetched featured books

--- src/context/CardsContext.js
+++ src/context/CardsContext.js
@@ -1,3 +1,28 @@
-import {React,createContext,useEffect,useNavigate} from 'react';
+import {Children, React,createContext,useEffect,useNavigate} from 'react';
+import fetchData from '../utility/fetch';
 
-const CardsContext = createContext();
+export const CardsContext = createContext();
+
+export const CradProvider = ({children}) => { 
+
+const getFeatured = async()=>{
+    const base_url = process.env.BASE_URL;
+    try{
+        const response = await fetchData(
+            `${base_url}/getFeaturedBooks`,
+            "POST",
+            null,
+        )
+        console.log(response)
+    }catch(error){
+        console.log(error.response.error.message)
+    }
+} 
+    return(
+        <CardsContext.Provider value={{
+            getFeatured
+        }}> 
+            {children}
+        </CardsContext.Provider>
+    )
+}
