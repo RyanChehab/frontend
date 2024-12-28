bc52bd13621f374cd20d0d933a441a73053ff4de
RyanChehab <ryanchehab20@gmail.com>
2024-12-28 14:49:09 +0200

    jsx <=> debugged

--- src/components/Books/BooksDisplay/index.jsx
+++ src/components/Books/BooksDisplay/index.jsx
@@ -1,13 +1,28 @@
-import React,{useContext} from "react";
+import React,{useContext,useState} from "react";
 import { CardsContext } from "../../../context/CardsContext";
 import BookCard from "../BookCard";
 
 const DisplayCards = ()=>{
-    const{data} = useContext(CardsContext)
+    const { data, loading} = useContext(CardsContext);
+    const booksArray = Object.values(data);
 
-    return(
+     console.log("Books Array:", booksArray);
+     return loading ? (
+        <p>Loading...</p>
+      ) : booksArray.length > 0 ? (
         <div className="books-container">
-            
+          {booksArray.map((book, index) => (
+            <BookCard
+              key={index}
+              img_url={book.img_url}
+              title={book.title}
+              author={book.author}
+            />
+          ))}
         </div>
-    )
-}
+      ) : (
+        <p>No books available</p>
+      );
+};
+
+export default DisplayCards;
