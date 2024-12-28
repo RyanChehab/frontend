e1f172175371470b3ccbcd58538106b90a6e691e
RyanChehab <ryanchehab20@gmail.com>
2024-12-28 03:35:39 +0200

    jsx <=> destructured data from CardsContext

--- src/components/Books/BooksDisplay/index.jsx
+++ src/components/Books/BooksDisplay/index.jsx
@@ -2,3 +2,12 @@ import React,{useContext} from "react";
 import { CardsContext } from "../../../context/CardsContext";
 import BookCard from "../BookCard";
 
+const DisplayCards = ()=>{
+    const{data} = useContext(CardsContext)
+
+    return(
+        <div className="books-container">
+            
+        </div>
+    )
+}
