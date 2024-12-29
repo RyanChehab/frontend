714030cf69724fa4365b41a8104c5a40cd8ca78c
RyanChehab <ryanchehab20@gmail.com>
2024-12-29 18:09:48 +0200

    jsx <=> added toggleBookmark function

--- src/components/Books/BookCard/index.jsx
+++ src/components/Books/BookCard/index.jsx
@@ -1,12 +1,26 @@
 import React,{useState} from "react";
 import './BookCard.css';
 import fetchData from "../../../utility/fetch";
-
 const BookCard = ({img_url, title, author, userBookmarks})=>{
 
     const [isBookmarked, setIsBookmarked] = useState(
         userBookmarks.some((bookmark) => bookmark.bookmarkable_id === id)
     );
+
+    const toggleBookmark = () => {
+        try{
+            fetchData(
+                "http://localhost:8000/api/bookmark",
+                "POST",
+                {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            )
+        }catch(error){
+            console.error("Error fetching dataa:", error)
+        }
+    };
+
 
     return(
         <div className="book-card">
