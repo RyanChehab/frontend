73063c2e1ee807317985176e110b1528028a0de2
RyanChehab <ryanchehab20@gmail.com>
2024-12-29 17:48:01 +0200

    jsx <=> passed Bookmark to Bookcard component

--- src/components/Books/BooksDisplay/index.jsx
+++ src/components/Books/BooksDisplay/index.jsx
@@ -1,15 +1,20 @@
 import React,{useContext,useState} from "react";
 import { CardsContext } from "../../../context/CardsContext";
 import BookCard from "../BookCard";
+import './BooksDisplay.css'
 
 const DisplayCards = ()=>{
-    const { data, loading} = useContext(CardsContext);
+    const { data, loading, bookmarks} = useContext(CardsContext);
+
+    
     const booksArray = Object.values(data);
 
      console.log("Books Array:", booksArray);
      return loading ? (
         <p>Loading...</p>
       ) : booksArray.length > 0 ? (
+      <>
+        <p className="display-title">Fetured Books</p>
         <div className="books-container">
           {booksArray.map((book, index) => (
             <BookCard
@@ -17,11 +22,13 @@ const DisplayCards = ()=>{
               img_url={book.img_url}
               title={book.title}
               author={book.author}
+              userBookmarks={bookmarks}
             />
           ))}
         </div>
+      </>
       ) : (
-        <p>No books available</p>
+        <p className="display-title">No books available</p>
       );
 };
 
