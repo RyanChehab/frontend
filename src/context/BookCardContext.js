d1c20ae9daf4bbaece103d1086cb4e7f34e08c6c
RyanChehab <ryanchehab20@gmail.com>
2024-12-30 21:26:32 +0200

    js <=> added toggleBookmark function

--- src/context/BookCardContext.js
+++ src/context/BookCardContext.js
@@ -1,1 +1,40 @@
-import React,{useEffect,useState} from "react";
+import React,{useEffect,useState,createContext} from "react";
+
+const BookCardContext = createContext()
+
+const BookCardProvider = ({children})=>{
+    
+    const navigate = useNavigate();
+    const handleNavigate = () =>{
+        navigate(`view/${gutenberg_id}`)
+    }
+    const [isBookmarked, setIsBookmarked] = useState(
+        userBookmarks.some((bookmark) => bookmark.bookmarkable_id === id)
+    );
+
+    const toggleBookmark = async () => {
+        try{
+            const result = await fetchData(
+                "http://localhost:8000/api/bookmark",
+                "POST",
+                {
+                    bookmarkable_id: id,
+                    bookmarkable_type: "App\\Models\\Book",
+                },
+                {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            )
+            const data = await result.json();
+
+            if (result.ok) {
+                setIsBookmarked(data.status); // Update the bookmark state
+            } else {
+                console.error("Failed to toggle bookmark:", data.message);
+            }
+            
+        }catch(error){
+            console.error("Error fetching dataa:", error)
+        }
+    };
+}
