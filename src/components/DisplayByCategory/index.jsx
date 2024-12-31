cfc743f43a332ed691cab9f1d14d14329b383fca
RyanChehab <ryanchehab20@gmail.com>
2024-12-31 16:08:52 +0200

    js <=> exported DisplayByCategory

--- src/components/DisplayByCategory/index.jsx
+++ src/components/DisplayByCategory/index.jsx
@@ -1,2 +1,12 @@
-import React from 'react';
+import React,{useContext}from 'react';
 import BookCard from '../Books/BookCard';
+import { CardsContext } from '../../context/CardsContext';
+
+const DisplayByCategory = ()=>{
+    const{category} = useContext(CardsContext);
+
+    const BooksArray = Object.values(category);
+    console.log("category:", category)
+}
+
+export default DisplayByCategory;
