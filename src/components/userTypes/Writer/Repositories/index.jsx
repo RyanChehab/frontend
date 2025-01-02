0e27964c24bb6f6471af7093475f83c4e3f1b8ed
RyanChehab <ryanchehab20@gmail.com>
2025-01-02 05:23:10 +0200

    jsx <=> created Repository container

---
+++ src/components/userTypes/Writer/Repositories/index.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {styled} from "@mui/material/styles"
+
+const RepositoriesContainer = styled("div")({
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    justifyContent: "center",
+    padding: "20px",
+})
