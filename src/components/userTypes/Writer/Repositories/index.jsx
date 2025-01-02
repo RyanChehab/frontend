a84059d76977e2b9cee549f478cc254ca758b31d
RyanChehab <ryanchehab20@gmail.com>
2025-01-02 19:13:13 +0200

    jsx <=> created FormContainer

--- src/components/userTypes/Writer/Repositories/index.jsx
+++ src/components/userTypes/Writer/Repositories/index.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import {styled} from "@mui/material/styles"
 import Button from "@mui/material/Button";
+import WriterNav from '../writerNav';
+import { RepositoryContext } from '../../../../context/RepositoryContext';
 
 const RepositoriesContainer = styled("div")({
     display: "flex",
@@ -32,37 +34,68 @@ const RepositoryCard = styled("div")({
     height: "30px",
     padding: "0",
     borderRadius: "50%",
-    backgroundColor: "#007bff",
+    backgroundColor: "#FC8E40",
     color: "#fff",
     "&:hover": {
       backgroundColor: "#0056b3",
     },
   });
 
+  const FormContainer = styled("div")({
+    width: "300px",
+    padding: "20px",
+    backgroundColor: "#fff",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+});
+
+  const ModalOverlay = styled("div")({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backdropFilter: "blur(5px)", 
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1000,
+  });
+
+
   const Repositories = () => {
+
+    const {handleAddRepository,showForm,setShowForm} = useContext(RepositoryContext)
     const repositories = [
         { id: 1, name: "Adventure Tales" },
         { id: 2, name: "Harry and the lost wand" },
     ];
 
-    const handleAddRepository = () => {
-        console.log("working!");
-    };
-
     return (
+    <>
+      <WriterNav/>
       <RepositoriesContainer>
       {repositories.map((repo) => (
           <RepositoryCard key={repo.id}>
               {repo.name}
               <AddButton onClick={() => console.log(`Editing ${repo.name}`)}>
-                  
+              <i class="fas fa-pencil-alt"></i>
               </AddButton>
           </RepositoryCard>
       ))}
       <RepositoryCard onClick={handleAddRepository}>
           <span>+ Add Repository</span>
       </RepositoryCard>
-  </RepositoriesContainer>
+      </RepositoriesContainer>
+      {showForm &&(
+        <FormContainer>
+          <ModalOverlay>
+          <p>test</p>
+          </ModalOverlay>
+        </FormContainer>
+      )}
+    </>
     );
 };
 
