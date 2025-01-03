04f90326ac4aa131068987408dd90309bd2c95a9
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 16:05:17 +0200

    jsx <=> rendered DeleteUser

--- src/components/userTypes/Admin/AdminLayout/index.jsx
+++ src/components/userTypes/Admin/AdminLayout/index.jsx
@@ -1,11 +1,30 @@
 import React, { useContext } from "react";
 import { AdminContext } from "../../../../context/AdminContext";
 import AdminNav from "../AdminNav";
+import AdminBlocklist from "../AdminBlocklist";
+import AdminDeletelist from "../AdminDelete";
+
 const Admin = ()=>{
-    const{password,setPassword,response,loading,handleBlockUser} = useContext(AdminContext)
+    const{password,setPassword,response,loading,handleBlockUser,activeMenuItem,setActiveMenuItem} = useContext(AdminContext)
     
+    const renderComponent = () => {
+        switch (activeMenuItem) {
+            case "blockUser":
+                return <AdminBlocklist />;
+            case "deleteUser":
+                return <AdminDeletelist/>;
+            // case "addAdmin":
+            //     return <div>Add Admin Component</div>;        
+        }
+    }
+
     return(
-        <AdminNav/>
+        <>
+        <AdminNav onSelectItem={setActiveMenuItem} />
+        <div>
+            {renderComponent()}
+        </div>
+        </>
     )
 }
 
