6af03eeb75b1e26b26143f69e2c7864b194a0548
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 02:12:01 +0200

    js <=> returned provider

--- src/context/AdminContext.js
+++ src/context/AdminContext.js
@@ -1,7 +1,33 @@
 import React,{useEffect,useState,createContext} from 'react';
-
+import fetchData from '../utility/fetch';
 export const AdminContext = createContext();
 
 export const AdminProvider = ({children}) => {
 
+    const [loading,setLoading] = useState(false)
+    const [response,setResponse] = useState()
+
+    // functions
+    const handleBlockUser = ()=>{
+        try{
+            setLoading(true)
+            fetchData(
+                "http://localhost:8000/api/block_user",
+                "POST",
+                {email}
+            )
+        }catch(error){
+
+        }finally{
+            
+        }
+    }
+
+    return(
+        <AdminContext.Provider value={{
+
+        }}>
+            {children}
+        </AdminContext.Provider>
+    )
 }
