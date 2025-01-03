2e3b5b04d434f2256bc7fff07ee638dd7061d227
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 15:48:57 +0200

    jsx <=> created AdminDelete component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Signup from "./components/Auth/signup";
 import Writer from "./components/userTypes/Writer";
 import Repositories from "./components/userTypes/Writer/Repositories";
 import DisplayByCategory from "./components/userTypes/Writer/DisplayByCategory";
+import Admin from "./components/userTypes/Admin/AdminLayout";
 import { AuthProvider } from "./context/AuthContext";
 import { NavProvider } from "./context/NavContext";
 import { CardProvider } from "./context/CardsContext";
 import { BookCardProvider } from "./context/BookCardContext";
 import { RepositoryProvider } from "./context/RepositoryContext";
 import { AdminProvider } from "./context/AdminContext";
+import { AdminListProvider } from "./context/AdminListContext";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
     <AuthProvider>
       <RepositoryProvider>
         <AdminProvider>
+          <AdminListProvider>
       <NavProvider>
         <CardProvider>
           <BookCardProvider>
@@ -25,10 +28,12 @@ function App() {
             <Route path="writer" element={<Writer/>}/>
             <Route path="Categories" element={<DisplayByCategory/>} />
             <Route path="repo" element={<Repositories/>}/>
+            <Route path="adminPanel" element={<Admin/>}/>
           </Routes>
           </BookCardProvider>
         </CardProvider>
       </NavProvider>
+      </AdminListProvider>
       </AdminProvider>
       </RepositoryProvider>
     </AuthProvider>
---
+++ src/components/userTypes/Admin/AdminDelete/index.jsx
@@ -0,0 +1,3 @@
+import React,{useContext} from "react";
+import { styled } from "@mui/system";
+import { AdminListContext } from "../../../../context/AdminListContext";
