7492e21a3bb1f0d4ddd0b6c494a755a8ba815abc
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 06:05:26 +0200

    js <=> fetched all users

--- src/context/AdminListContext.js
+++ src/context/AdminListContext.js
@@ -1,14 +1,36 @@
 import React,{useEffect,useState,createContext} from "react";
+import fetchData from '../utility/fetch';
 
 export const AdminListContext = createContext();
 
 export const AdminListProvider = ({children}) => {
 
+    const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(false);
+
+    useEffect(()=>{
+
+        const getUsers = async () => {
+            try{
+                const response = await fetchData(
+                    "http://localhost:8000/api/auth/getUsers",
+                    "POST"
+                )
+                console.log(response.data)
+                setUsers(response.data)
+            }catch(error){
+                console.error(error)
+            }
+        }
+
+        getUsers();
+
+    },[])
     return(
-        <AdminListContext value={{
-            
+        <AdminListContext.Provider value={{
+            users
         }}>
             {children}
-        </AdminListContext>
+        </AdminListContext.Provider>
     )
 }
