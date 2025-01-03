ec37455116ed2e4a315339d469405386b9c7fc2f
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 02:17:21 +0200

    js <=> routed and provided context temporarily

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import Login from "./components/login";
-import Signup from "./components/signup";
+import Login from "./components/Auth/login";
+import Signup from "./components/Auth/signup";
+import Writer from "./components/userTypes/Writer";
+import Repositories from "./components/userTypes/Writer/Repositories";
+import DisplayByCategory from "./components/userTypes/Writer/DisplayByCategory";
 import { AuthProvider } from "./context/AuthContext";
+import { NavProvider } from "./context/NavContext";
+import { CardProvider } from "./context/CardsContext";
+import { BookCardProvider } from "./context/BookCardContext";
+import { RepositoryProvider } from "./context/RepositoryContext";
+import { AdminProvider } from "./context/AdminContext";
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
+    <Router>
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Routes>
-      </Router>
+      <RepositoryProvider>
+        <AdminProvider>
+      <NavProvider>
+        <CardProvider>
+          <BookCardProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="writer" element={<Writer/>}/>
+            <Route path="Categories" element={<DisplayByCategory/>} />
+            <Route path="repo" element={<Repositories/>}/>
+          </Routes>
+          </BookCardProvider>
+        </CardProvider>
+      </NavProvider>
+      </AdminProvider>
+      </RepositoryProvider>
     </AuthProvider>
+    </Router>
+    
   );
 }
-
 export default App;
