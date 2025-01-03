330e4e0d70ede7c7e6918204987f2aed0208162f
RyanChehab <ryanchehab20@gmail.com>
2025-01-03 13:37:08 +0200

    jsx <=> fixed list

--- src/components/userTypes/Admin/AdminBlocklist/index.jsx
+++ src/components/userTypes/Admin/AdminBlocklist/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React,{useContext} from "react";
 import { styled } from "@mui/system";
+import { AdminListContext } from "../../../../context/AdminListContext";
 
 const BlockUserContainer = styled("div")({
     padding: "20px",
@@ -25,3 +26,70 @@ const TableHeader = styled("thead")({
     top: 0,
     zIndex: 1,
 });
+
+const TableRow = styled("tr")({
+    "&:nth-of-type(even)": {
+        backgroundColor: "#f9f9f9",
+    },
+    "&:hover": {
+        backgroundColor: "#f1f1f1",
+    },
+});
+
+const TableHeaderCell = styled("th")({
+    padding: "10px",
+    fontWeight: "bold",
+    borderBottom: "2px solid #ddd",
+});
+
+const TableCell = styled("td")({
+    padding: "10px",
+    borderBottom: "1px solid #ddd",
+});
+
+const AdminBlocklist = () => {
+    const {users} = useContext(AdminListContext);
+
+    return (
+        <BlockUserContainer>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderCell>ID</TableHeaderCell>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell>Role</TableHeaderCell>
+                        <TableHeaderCell>Email</TableHeaderCell>
+                        <TableHeaderCell>Status</TableHeaderCell>
+                        <TableHeaderCell>Status</TableHeaderCell>
+                    </TableRow>
+                </TableHeader>
+                <tbody>
+                    {users.map((user) => (
+                        <TableRow key={user.id}>
+                            <TableCell>{user.id}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.user_type}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>
+                                {user.blocked ? (
+                                    <span style={{ color: "red" }}>Blocked</span>
+                                ) : (
+                                    <span style={{ color: "green" }}>Active</span>
+                                )}
+                            </TableCell>
+                            <TableCell>
+                                {user.blocked ? (
+                                    <span><button  style={{ color: "green"}}>Unblock</button></span>
+                                ) : (
+                                    <span><button style={{ color: "red" }}>block</button></span>
+                                )}
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </tbody>
+            </Table>
+        </BlockUserContainer>
+    );
+};
+
+export default AdminBlocklist;
