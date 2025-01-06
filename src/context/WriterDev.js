c29609a886b8017aa220aa1e6fa9d9d5f78d3872
RyanChehab <ryanchehab20@gmail.com>
2025-01-06 15:48:38 +0200

    js <=> added states

--- src/context/WriterDev.js
+++ src/context/WriterDev.js
@@ -1,7 +1,15 @@
 import { useState,createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const WriterDevContext = createContext()
 
 export const WriterDevProvider = () => {
+    const navigate = useNavigate()
+    const [fanfiction, setFanfiction] = useState(""); 
+    const [currentPage, setCurrentPage] = useState(1); 
+
+    const handleBack = () => {
+        navigate("/writer")
+    };
 
 }
