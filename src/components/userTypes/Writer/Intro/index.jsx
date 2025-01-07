ee66d498f60607004559f7bbb4c12068172d5158
RyanChehab <ryanchehab20@gmail.com>
2025-01-07 11:11:55 +0200

    jsx <=> imported useNavigate

--- src/components/userTypes/Writer/Intro/index.jsx
+++ src/components/userTypes/Writer/Intro/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 import './Intro.css'
 const Intro = () => {
 
+    const navigate = useNavigate()
     return(
         <div className="intro-container">
       <div className="intro-overlay"></div>
       <div className="intro-content">
         <h1 className="intro-heading">Unleash Your <span className="imagination">Imagination</span></h1>
         <p className="intro-subheading">
-          Every tale starts with a single word. Start yours now.
+          Every <span className="tale">tale</span> starts with a single word. Start yours now.
         </p>
-        <button className="intro-btn">
+        <button className="intro-btn" onClick={()=>{navigate('/repositories')}}>
           Explore Your Repositories
         </button>
       </div>
