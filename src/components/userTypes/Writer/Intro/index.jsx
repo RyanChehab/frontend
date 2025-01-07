64b8ff1e2d6e28f97d3d9dde789092d0f099118e
RyanChehab <ryanchehab20@gmail.com>
2025-01-07 10:28:40 +0200

    jsx <=> created intro component

--- src/components/userTypes/Writer/Intro/index.jsx
+++ src/components/userTypes/Writer/Intro/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import './Intro.css'
 const Intro = () => {
 
+    return(
+        <div className="intro-container">
+      <div className="intro-overlay"></div>
+      <div className="intro-content">
+        <h1 className="intro-heading">Unleash Your <span className="imagination">Imagination</span></h1>
+        <p className="intro-subheading">
+          Every tale starts with a single word. Start yours now.
+        </p>
+        <button className="intro-btn">
+          Explore Your Repositories
+        </button>
+      </div>
+    </div>
+    )
 }
 
-export default Intro
+export default Intro;
