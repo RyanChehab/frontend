b55f603be00f0f5578eb5a1f68dade631c7ae434
RyanChehab <ryanchehab20@gmail.com>
2025-01-09 21:40:00 +0200

    jsx <=> displayed writerNav

--- src/components/userTypes/Writer/Bookmarks/index.jsx
+++ src/components/userTypes/Writer/Bookmarks/index.jsx
@@ -2,7 +2,7 @@ import React,{useContext} from 'react'
 import BookCard from "../BookCard";
 import { CardsContext } from '../../../../context/CardsContext';
 import { useBookmark } from "../../../../context/BookCardContext";
-
+import WriterNav from '../writerNav';
 const Bookmarks = () => {
     const {data, loading} = useContext(CardsContext)
     const { isBookmarked, bookmarkedBooks } = useBookmark();  
@@ -10,13 +10,16 @@ const Bookmarks = () => {
     const books = Object.values(data);
 
     const bookmarked = books.filter(book => bookmarkedBooks.includes(book.gutenberg_id));
+    
 
     if (loading) return <p>Loading bookmarks...</p>;
     
     return(
+        <>
+        <WriterNav/>
         <div>
-            <h1>Bookmarked Books</h1>
-            <div className="bookmarks-container">
+            <h1 className="display-title">Bookmarked Books</h1>
+            <div className="books-container">
                 {bookmarked.length > 0 ? (
                     bookmarked.map(book => <BookCard 
                         gutenberg_id={book.gutenberg_id}
@@ -30,6 +33,7 @@ const Bookmarks = () => {
                 )}
             </div>
         </div>
+        </>
     )
 }
 
