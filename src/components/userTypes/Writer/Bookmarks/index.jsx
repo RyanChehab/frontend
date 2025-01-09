4916af059a19529304c042abde9c481d2b8847c9
RyanChehab <ryanchehab20@gmail.com>
2025-01-09 18:53:50 +0200

    jsx <=> created Bookmarks component

--- src/components/userTypes/Writer/Bookmarks/index.jsx
+++ src/components/userTypes/Writer/Bookmarks/index.jsx
@@ -3,4 +3,25 @@ import BookCard from "../BookCard";
 
 const Bookmarks = ({books}) => {
     const bookmarkedBooks = books.filter(book => book.isBookmarked);
+
+    return(
+        <div>
+            <h1>Bookmarked Books</h1>
+            <div className="bookmarks-container">
+                {bookmarkedBooks.length > 0 ? (
+                    bookmarkedBooks.map(book => <BookCard 
+                        gutenberg_id={book.gutenberg_id}
+                        img_url={book.img_url}
+                        title={book.title}
+                        author={book.author}
+                        isBookmarked={true}
+                         />)
+                ) : (
+                    <p>No bookmarks found.</p>
+                )}
+            </div>
+        </div>
+    )
 }
+
+export default Bookmarks;
