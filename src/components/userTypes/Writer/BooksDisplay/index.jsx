342ffe33ac3e58ecb2cdeddf71f0e3199f7bed6e
RyanChehab <ryanchehab20@gmail.com>
2025-01-09 21:32:05 +0200

    jsx <=> cleaned component

--- src/components/userTypes/Writer/BooksDisplay/index.jsx
+++ src/components/userTypes/Writer/BooksDisplay/index.jsx
@@ -1,34 +1,31 @@
 import React,{useContext,useState} from "react";
 import { CardsContext } from "../../../../context/CardsContext";
+import { useBookmark } from "../../../../context/BookCardContext";
 import BookCard from "../BookCard";
 import './BooksDisplay.css'
 
 const DisplayCards = ()=>{
-    const { data, loading} = useContext(CardsContext);
-    
+    const { data} = useContext(CardsContext);
+    const { isBookmarked, bookmarkedBooks } = useBookmark(); 
+    console.log("Bookmarked IDs:", bookmarkedBooks);
+    console.log(data)
     const booksArray = Object.values(data);
 
-     console.log("Books Array:", booksArray);
-     return loading ? (
-        <p className="display-loading-message">Loading...</p>
-      ) : booksArray.length > 0 ? (
+     return (
       <>
-        <h2 className="display-title">Fetured Books</h2>
-        <div className="books-container">
-          {booksArray.map((book, index) => (
+        <div>
+          {booksArray.map((book) => (
             <BookCard
               gutenberg_id = {book.gutenberg_id}
               img_url={book.img_url}
               title={book.title}
               author={book.author}
-              
+              isBookmarked={isBookmarked(book.gutenberg_id)}
             />
           ))}
         </div>
       </>
-      ) : (
-        <p className="display-title">No books available</p>
-      );
+      ) 
 };
 
 export default DisplayCards;
