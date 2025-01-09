ec2b7730e594bc8330dc7b83954f1ba614f06505
RyanChehab <ryanchehab20@gmail.com>
2025-01-09 10:49:33 +0200

    jsx <=> debugged

--- src/components/userTypes/Writer/BookCard/index.jsx
+++ src/components/userTypes/Writer/BookCard/index.jsx
@@ -1,15 +1,29 @@
 import React,{useState,useContext} from "react";
 import { useNavigate } from "react-router-dom";
-import { BookCardContext } from "../../../../context/BookCardContext";
+import { useBookmark } from "../../../../context/BookCardContext";
 import './BookCard.css';
 
 const BookCard = ({ gutenberg_id, img_url, title, author})=>{
+    const { handleBookmark, removeBookmark, isBookmarked } = useBookmark();
+    
+    const handleBookmarkToggle = () => {
+        if (isBookmarked(gutenberg_id)) {
+            // removeBookmark(gutenberg_id);
+            console.log('ok remove')
+        } else {
+            handleBookmark(gutenberg_id);
+        }
+    };
 
     return(
         <div className="book-card">
             
-            <button className="bookmark-btn" >
-                <i className="far fa-bookmark"></i>
+            <button
+                className={`bookmark-btn ${isBookmarked(gutenberg_id) ? "bookmarked" : ""}`}
+                onClick={handleBookmarkToggle}
+            >
+                {/* Dynamic Bookmark Icon */}
+                <i className={isBookmarked(gutenberg_id) ? "fas fa-bookmark" : "far fa-bookmark"}></i>
             </button>
 
             <div className="book-image-container">
