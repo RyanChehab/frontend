01cb9a55d86736781bfa0ecee276199b5941b457
RyanChehab <ryanchehab20@gmail.com>
2025-01-14 16:02:55 +0200

    js <=> renamed GutenBookContext

--- src/context/GutenBookContext.js
+++ src/context/GutenBookContext.js
@@ -1,7 +1,32 @@
 import React,{createContext} from "react";
+import fetchData from "../utility/fetch";
 
-export const GutenBookContext = createContext()
+export const BookContext = createContext()
 
-export const GutenBookProvider = ({children}) => {
+export const BookProvider = ({children}) =>{
 
-} 
+    const token = localStorage.getItem('token')
+
+    const fetchBookContent = async (url) => {
+        try {
+            const response = await fetchData(
+                'http://localhost:8000/api/book/fetchBookContent', 
+                'POST',
+                {url: url},
+                {Authorization: `Bearer: ${token}`},
+                
+            );
+            console.log(response)
+        } catch (error) {
+            console.error("Failed to fetch the book content:", error);
+        }
+    };
+
+    return(
+        <BookContext.Provider value={{
+            fetchBookContent
+        }}>
+            {children}
+        </BookContext.Provider>
+    )
+}
