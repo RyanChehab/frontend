7ed0bffcf64d7ddfd1dd6527f15ecb23dc02b588
RyanChehab <ryanchehab20@gmail.com>
2025-01-15 21:15:32 +0200

    jsx <=> imported styled components

--- src/components/userTypes/Reader/Repositories/index.jsx
+++ src/components/userTypes/Reader/Repositories/index.jsx
@@ -1,7 +1,14 @@
 import React,{useContext}from "react";
 import { RepositoryContext } from "../../../../context/RepositoryContext";
+import { RepositoriesContainer } from "../../Writer/Repositories";
+import { RepositoryCard } from "../../Writer/Repositories";
+import { FormContainer } from "../../Writer/Repositories";
+import { CardImage } from "../../Writer/Repositories";
+import { CardDetails } from "../../Writer/Repositories";
+import { ModalOverlay } from "../../Writer/Repositories";
 
 const ReaderRepository = () => {
+    const {repositories} = useContext(RepositoryContext)
 
     return()
 } 
