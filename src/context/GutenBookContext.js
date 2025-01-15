910dc9fd846a40a573f59eb81d0c2686c42e19e3
RyanChehab <ryanchehab20@gmail.com>
2025-01-15 03:22:33 +0200

    js <=> added handleCreate

--- src/context/GutenBookContext.js
+++ src/context/GutenBookContext.js
@@ -19,6 +19,14 @@ export const GutenBookProvider = ({children}) =>{
     const [forkMode, setForkMode] = useState(false);
     const [forkedContent, setForkedContent] = useState("");
 
+    // creating repo
+    const [showForm,setShowForm] = useState(false)
+    const [repoLoading,setRepoLoading] = useState(false)
+    const [open,setOpen] = useState(false)
+    const [response,setResponse] = useState('')
+    const [type,setType] = useState('')
+    const [description,setDescription] = useState('')
+
     const token = localStorage.getItem('token')
 
     const fetchBookContent = async (url,title) => {
@@ -102,10 +110,79 @@ export const GutenBookProvider = ({children}) =>{
         const adjustedCursorPosition = currentPageStart + cursorPositionOnPage;
 
         const forkedContent = fullContent.slice(0, adjustedCursorPosition);
-
+        console.log(forkedContent)
         setForkedContent(forkedContent); // Save the sliced content
         setForkMode(false); // Exit fork mo
+
+        setShowForm(true)
     };
+
+
+    const handleCreateRepository = async (e) => {
+        e.preventDefault()
+        try{
+            setRepoLoading(true);
+
+            // creating the repository 
+            const response = await fetchData(
+                "http://localhost:8000/api/createRepo",
+                "POST",
+                
+                {title,description},
+
+                {Authorization: `Bearer ${token}`}
+            )
+            // retrieve repo id 
+            const repositoryId = response.data.id
+            
+            // prepend fixed string to the description 
+            const prompt = `Create book cover (only need the cover photo). The theme is based on the following fanfiction description: ${description}`
+
+            // second api fetch
+            const imageResponse = await fetchData(
+                "http://localhost:8000/api/generate-image",
+                "POST",
+                {prompt},
+                {Authorization: `Bearer ${token}`}
+            )
+
+            // save the retrieved path
+            const s3url = imageResponse.s3url;
+            if(!s3url){
+                console.log('failed to generate img')
+            }
+            // third api fetch
+            const updateImg = await fetchData(
+                `http://localhost:8000/api/updateRepo/${repositoryId}`,
+                'POST',
+                {s3url},
+                {Authorization: `Bearer ${token}`}
+            )
+
+            if(response.message === 'Repository created successfully!'){
+
+                navigate(`WriterDev/${repositoryId}`)
+            setType('success')
+               setResponse(response.message)
+            setShowForm(false)
+            }
+
+            
+        }catch(error){
+            setResponse(error.response.data.message)
+            setType('error')
+        }finally{
+            setRepoLoading(false)
+            setOpen(true)
+        }
+    }
+
+
+
+    const handleCloseNotification = () => {
+        setOpen(false);
+      };
+
 
     return(
         <GutenBookContext.Provider value={{
@@ -117,7 +194,16 @@ export const GutenBookProvider = ({children}) =>{
             loading,
             title,
             handleFork,
-            activateForkMode
+            activateForkMode,
+            forkMode,
+            handleCreateRepository,
+            showForm,
+            setShowForm,
+            open,
+            handleCloseNotification,
+            type,
+            setTitle,
+            setDescription
         }}>
             {children}
         </GutenBookContext.Provider>
