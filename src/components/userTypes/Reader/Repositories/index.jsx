41ceec13d632b0ced9fa1ef4147fdad5f19dc471
RyanChehab <ryanchehab20@gmail.com>
2025-01-15 21:21:28 +0200

    jsx <=> built repo card layout

--- src/components/userTypes/Reader/Repositories/index.jsx
+++ src/components/userTypes/Reader/Repositories/index.jsx
@@ -2,14 +2,29 @@ import React,{useContext}from "react";
 import { RepositoryContext } from "../../../../context/RepositoryContext";
 import { RepositoriesContainer } from "../../Writer/Repositories";
 import { RepositoryCard } from "../../Writer/Repositories";
-import { FormContainer } from "../../Writer/Repositories";
 import { CardImage } from "../../Writer/Repositories";
 import { CardDetails } from "../../Writer/Repositories";
-import { ModalOverlay } from "../../Writer/Repositories";
 
 const ReaderRepository = () => {
     const {repositories} = useContext(RepositoryContext)
 
-    return()
-} 
+    return(
+        <RepositoriesContainer>
+            {repositories.map((repo) => (
+                <RepositoryCard key={repo.id}>
+                    {/* Full-Card Background Image */}
+                    <CardImage
+                        style={{ backgroundImage: `url(${repo.img_url})` }}
+                    ></CardImage>
+
+                    {/* Details Section */}
+                    <CardDetails className="card-details">
+                        <h3>{repo.title}</h3>
+                        <p>{repo.description}</p>
+                    </CardDetails>
+                </RepositoryCard>
+            ))}
+        </RepositoriesContainer>
+)
+}
 export default ReaderRepository;
