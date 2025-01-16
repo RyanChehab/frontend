475d669aebd2132d3c2449a05e2290b8a0ce15d8
RyanChehab <ryanchehab20@gmail.com>
2025-01-16 23:04:48 +0200

    js <=> created ReaderRepoContext

--- src/context/ReaderBookmarkContext.js
+++ src/context/ReaderBookmarkContext.js
@@ -1,2 +1,10 @@
-import React, {createContext} from "react";
+import React, {createContext,useState} from "react";
 import fetchData from "../utility/fetch";
+
+export const ReaderBookmarkContext = createContext()
+
+export const ReaderBookmarkProvider = () =>{
+
+    const [bookmarkedRepo,setBookmarkedRepo] = useState([])
+}
+
