6615eaa44728cb15abbe632ad29b38311719368d
RyanChehab <ryanchehab20@gmail.com>
2025-01-16 23:50:01 +0200

    js <=> added getBookmarks

--- src/context/ReaderBookmarkContext.js
+++ src/context/ReaderBookmarkContext.js
@@ -5,6 +5,27 @@ export const ReaderBookmarkContext = createContext()
 
 export const ReaderBookmarkProvider = () =>{
 
+    const token = localStorage.getItem('token')
     const [bookmarkedRepo,setBookmarkedRepo] = useState([])
+
+    useEffect(()=>{
+        const getBookmarks = async () => {
+            try{
+                const result = await fetchData(
+                    "http://localhost:8000/api/bookmarks/getBookmarks",
+                    "POST",
+                    null,
+                    {Authorization: `Bearer ${token}`}
+                )
+                setBookmarkedRepo(result.bookmarked_ids);
+            }catch(error){
+                console.error(error);
+            }
+        }
+        if (token) {
+            getBookmarks();
+        }
+        
+    },[token])
 }
 
