e6797665064cc27ec9c68ad13238434ec4a8cf4f
RyanChehab <ryanchehab20@gmail.com>
2025-01-16 23:08:57 +0200

    js <=> fetched repositories

--- src/context/ReaderRepositoryContext.js
+++ src/context/ReaderRepositoryContext.js
@@ -5,7 +5,20 @@ export const ReaderRepositoryContext = createContext()
 
 export const ReaderRepoProvider = ({children}) => {
 
-
+    // fetching all repos
+    useEffect(()=>{
+        const repos = async () => {
+            try{
+                const result = await fetchData(
+                    'http://localhost:8000/api/getReaderRepositories',
+                    'POST',
+                    null,
+                    {Authorization: `Bearer ${token}`}
+                )
+                console.log("reader repo: ", result)
+            }
+        }
+    },[])
     return(
         <ReaderRepositoryContext.Provider value={{}}>
             {children}
