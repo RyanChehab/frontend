74790cda9f4c221e6206c1fde4a9374d4f353bc3
RyanChehab <ryanchehab20@gmail.com>
2025-01-17 01:42:43 +0200

    jsx <=> used ReaderRepositoryContext

--- src/components/userTypes/Reader/DisplayRepo/index.jsx
+++ src/components/userTypes/Reader/DisplayRepo/index.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
-    
+import ReaderCard from "../RepositoryCard";
 import { ReaderBookmarkContext } from "../../../../context/ReaderBookmarkContext";
 import { ReaderRepositoryContext } from "../../../../context/ReaderRepositoryContext";
 
+
 const DisplayRepos = () => {
     
-    const {repositories} = useContext(ReaderRepositoryContext)
-
+    const {repositories,loading} = useContext(ReaderRepositoryContext)
+    // const {isBookmarked} = useContext(ReaderBookmarkContext)
 }
 
 export default DisplayRepos;
