e7335f19308dff766299bc031b2ecbe3a2b97ce3
RyanChehab <ryanchehab20@gmail.com>
2025-01-17 01:39:33 +0200

    js <=> added loading state

--- src/context/ReaderRepositoryContext.js
+++ src/context/ReaderRepositoryContext.js
@@ -1,13 +1,18 @@
-import React,{useEffect,createContext} from "react";
+import React,{useEffect,createContext, useState} from "react";
 import fetchData from "../utility/fetch";
 
 export const ReaderRepositoryContext = createContext()
 
 export const ReaderRepoProvider = ({children}) => {
 
+    const token = localStorage.getItem('token')
+    const [repositories,setRepos] = useState([]);
+    const [loading,setLoading] = useState(true)
+
     // fetching all repos
     useEffect(()=>{
         const repos = async () => {
+            console.log('hellooooo')
             try{
                 const result = await fetchData(
                     'http://localhost:8000/api/getReaderRepositories',
@@ -16,11 +21,21 @@ export const ReaderRepoProvider = ({children}) => {
                     {Authorization: `Bearer ${token}`}
                 )
                 console.log("reader repo: ", result)
+                setRepos(result.repositories)
+            }catch(error){
+                console.log(error)
+            }finally{
+                setLoading(false)
             }
         }
+
+        repos()
     },[])
     return(
-        <ReaderRepositoryContext.Provider value={{}}>
+        <ReaderRepositoryContext.Provider value={{
+            repositories,
+            loading
+        }}>
             {children}
         </ReaderRepositoryContext.Provider>
     )
