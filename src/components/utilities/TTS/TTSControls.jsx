7915c60450d708c2f17535bb29a31ed5f1f035ca
RyanChehab <ryanchehab20@gmail.com>
2025-01-17 09:37:23 +0200

    jsx <=> added icons for text to speech controls

--- src/components/utilities/TTS/TTSControls.jsx
+++ src/components/utilities/TTS/TTSControls.jsx
@@ -1,22 +1,37 @@
 import React, {useContext} from "react";
 import { TTSContext } from "./TtsContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faStop, faRedo } from "@fortawesome/free-solid-svg-icons";
+import './TTS.css';
 
 const TTSControls = ({ text }) => {
 const { isSpeaking, isPaused, playTTS, pauseTTS, resumeTTS, stopTTS } = useContext(TTSContext);
 
     return (
         <div className="tts-controls">
-        <button onClick={() => playTTS(text)} disabled={isSpeaking && !isPaused}>
-            Play
+            
+            {/* play btn  */}
+        <button className="tts-button" onClick={() => playTTS(text)} disabled={isSpeaking && !isPaused}>
+        <FontAwesomeIcon icon={faPlay} />
+        <span className="tooltip">Play</span>
         </button>
-        <button onClick={pauseTTS} disabled={!isSpeaking || isPaused}>
-            Pause
+
+            {/* Pause btn */}
+        <button className="tts-button" onClick={pauseTTS} disabled={!isSpeaking || isPaused}>
+                <FontAwesomeIcon icon={faPause} />
+                <span className="tooltip">Pause</span>
         </button>
-        <button onClick={resumeTTS} disabled={!isPaused}>
-            Resume
+         
+            {/* resume btn  */}
+        <button className="tts-button" onClick={resumeTTS} disabled={!isPaused}>
+            <FontAwesomeIcon icon={faRedo} />
+            <span className="tooltip">Resume</span>
         </button>
-        <button onClick={stopTTS} disabled={!isSpeaking}>
-            Stop
+
+
+        <button className="tts-button" onClick={stopTTS} disabled={!isSpeaking}>
+            <FontAwesomeIcon icon={faStop} />
+            <span className="tooltip">Stop</span>
         </button>
         </div>
     );
