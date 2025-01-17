1aad2ee86ceb39cc8540e67f9e648c4f5bd53d22
RyanChehab <ryanchehab20@gmail.com>
2025-01-17 02:11:39 +0200

    jsx <=> mapped over ReaderCard

--- src/components/userTypes/Reader/DisplayRepo/index.jsx
+++ src/components/userTypes/Reader/DisplayRepo/index.jsx
@@ -8,6 +8,24 @@ const DisplayRepos = () => {
     
     const {repositories,loading} = useContext(ReaderRepositoryContext)
     // const {isBookmarked} = useContext(ReaderBookmarkContext)
+
+    return loading ? (
+        <p className="display-loading-message">Loading...</p>
+    ):
+    (<>
+        <p className="display-title">Featured Repositories</p>
+        <div className="books-container">
+            {repositories.map((repo) =>(
+                <ReaderCard
+                id = {repo.id}
+                title = {repo.title}
+                isBookmarked = {repo.isBookmarked}
+                img_url = {repo.img_url}
+                />
+            ))}
+        </div>
+    </>
+    )
 }
 
 export default DisplayRepos;
