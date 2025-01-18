ea4dbe1b1b36d0dc492a0f76f2778380850986d0
RyanChehab <ryanchehab20@gmail.com>
2025-01-18 13:45:40 +0200

    jsx <=> displayd intro

--- src/components/userTypes/Reader/index.jsx
+++ src/components/userTypes/Reader/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReaderNav from "./Nav";
 import DisplayRepos from "./DisplayRepo";
+import IntroBanner from "./Intro";
+
 const Reader = () => {
     return(
     <>
         <ReaderNav/>
+        <IntroBanner/>
         <DisplayRepos/>
     </>
     )
