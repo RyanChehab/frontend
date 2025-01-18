d20803b07a76d5a7dfe89f757af073b9e708425e
RyanChehab <ryanchehab20@gmail.com>
2025-01-18 13:46:16 +0200

    jsx <=> created banner for reader

--- src/components/userTypes/Reader/Intro/index.jsx
+++ src/components/userTypes/Reader/Intro/index.jsx
@@ -1,1 +1,27 @@
 import React from "react";
+
+import "./IntroBanner.css"; // Add corresponding CSS file
+
+const IntroBanner = () => {
+  return (
+    <div className="intro-banner">
+      {/* Left Section */}
+      <div className="intro-left">
+        <h1 className="intro-title">Finals Exam</h1>
+        <p className="intro-description">A teacher gets shocking news that will ruin his life</p>
+        <button className="visit-repo-btn">
+          Visit Repository 
+        </button>
+      </div>
+
+      {/* Right Section */}
+      <div className="intro-right">
+        <div className="card-image">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntroBanner;
