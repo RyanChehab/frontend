699e5db6eb6829a0ccd1718a0f3e2ad5d8d38ec4
RyanChehab <ryanchehab20@gmail.com>
2025-01-19 11:16:31 +0200

    js <=> setUsertype

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,9 +1,17 @@
-import React, {createContext} from 'react'
+import React, {useState,createContext} from 'react'
 
 const UserContext = createContext()
 
 const UserProvider = ({children}) => {
-    
+
+    const [usertype,setUsertype] = useState('');
+    const [show,setShow] = useState(false)
+
+    setUsertype(localStorage.getItem('type'))
+
+    if (usertype==='writer'){
+        setShow(true);
+    }
 
     return (<UserContext value={{}}>
         {children}
