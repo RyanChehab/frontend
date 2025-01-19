399159b733b88c5ea302f9ada0bf05b1a2ac1457
RyanChehab <ryanchehab20@gmail.com>
2025-01-19 11:46:39 +0200

    jsx <=> debugged

--- src/components/userTypes/Reader/ReaderBooks/index.jsx
+++ src/components/userTypes/Reader/ReaderBooks/index.jsx
@@ -3,10 +3,13 @@ import DisplayCards from "../../Writer/BooksDisplay";
 import ReaderNav from "../Nav";
 
 const ReaderBooks = () => {
-    <>
+    
+    return(
+        <>
         <ReaderNav/>
         <DisplayCards/>
     </>
+    )
 }
 
 export default ReaderBooks;
