cc48bde994360394921237ce5367d05b651076d7
RyanChehab <ryanchehab20@gmail.com>
2025-01-19 12:52:05 +0200

    jsx <=> got content of repository

--- src/components/userTypes/Reader/Intro/index.jsx
+++ src/components/userTypes/Reader/Intro/index.jsx
@@ -1,13 +1,14 @@
 import React, {useContext} from "react";
 import { useNavigate } from "react-router-dom";
 import { IntroContext } from "../../../../context/IntroContext";
+import { WriterDevContext } from "../../../../context/WriterDev";
 import "./IntroBanner.css"; // Add corresponding CSS file
 
 const IntroBanner = () => {
 
   const{repository} = useContext(IntroContext)
-  const 
-  
+  const navigate = useNavigate()
+  const {handleGetContent} = useContext(WriterDevContext)
 
   if (!repository) {
     return <div>Loading...</div>;
@@ -20,8 +21,8 @@ const IntroBanner = () => {
         <h1 className="intro-title">{repository.title}</h1>
         <p className="intro-description">{repository.description}</p>
         <button className="visit-repo-btn" onClick={async ()=>{
-                            await handleGetContent(id)
-                            navigate(`/ReaderDev/${id}`,{state:{RepoTitle: title}})
+                            await handleGetContent(repository.id)
+                            navigate(`/ReaderDev/${repository.id}`,{state:{RepoTitle: title}})
                     }}>
           Visit Repository 
         </button>
