6e645aa3270cec949ed1400943e25ffe7251d05e
RyanChehab <ryanchehab20@gmail.com>
2025-01-19 11:43:31 +0200

    jsx <=> created ReaderBooks component

---
+++ src/components/userTypes/Reader/ReaderBooks/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DisplayCards from "../../Writer/BooksDisplay";
+import ReaderNav from "../Nav";
+
+const ReaderBooks = () => {
+    <>
+        <ReaderNav/>
+        <DisplayCards/>
+    </>
+}
+
+export default ReaderBooks;
