f7b7f21e65d89e1fc2db2a28ac2934066c456848
RyanChehab <ryanchehab20@gmail.com>
2025-01-20 15:56:20 +0200

    js <=> debugged state logic

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,28 +1,34 @@
 import React, {useState,createContext, useEffect} from 'react'
+import { useLocation } from 'react-router-dom'
 
 export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
-
-    const token = localStorage.getItem('token')
-    const [usertype,setUsertype] = useState('');
+    const location = useLocation()
+    const [initialized,setInitialized] = useState(false)
     const [show,setShow] = useState(false)
+
     
-    useEffect(()=>{
-
-        const userTypeStorage = localStorage.getItem('type')
+useEffect(() => {
+    const initializeState = async () => {
+        const userTypeStorage = localStorage.getItem('type');
         
-        setUsertype(userTypeStorage)
 
+        // Wait for `usertype` to update
         if (userTypeStorage === "writer") {
             setShow(true);
         } else {
             setShow(false);
         }
-    }, [token]);
+
+        setInitialized(true); 
+    };
+
+    initializeState();
+}, [location.pathname]);
 
 
-    return (<UserContext.Provider value={{show}}>
+    return (<UserContext.Provider value={{show,initialized}}>
         {children}
     </UserContext.Provider>)
 }
